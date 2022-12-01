84412be0772499a97ab95d9910f21d49ded351e4
AliAlkadhim <aa18dg@my.fsu.edu>
2022-12-01 17:24:09 -0500

    Braden scaling

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export IQN_BASE=/home/ali/Desktop/Pulled_Github_Repositories/torchQN
+#DAVIDSON
+#export DATA_DIR='/home/DAVIDSON/alalkadhim.visitor/IQN/DAVIDSON_NEW/data'
+#LOCAL
+export DATA_DIR='/home/ali/Desktop/Pulled_Github_Repositories/IQN_HEP/Davidson/data'
